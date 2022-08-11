65fb641b277e79fd31cc630ea26a934dde1a37b6
Marco Munizaga <git@marcopolo.io>
2022-08-11 10:52:49 -0700

    Fix usage of make to reserve capacity, not values

--- obs/stats_test.go
+++ obs/stats_test.go
@@ -2,9 +2,11 @@ package obs_test
 
 import (
 	"testing"
+	"time"
 
 	rcmgr "github.com/libp2p/go-libp2p-resource-manager"
 	"github.com/libp2p/go-libp2p-resource-manager/obs"
+	"go.opencensus.io/stats/view"
 )
 
 func TestTraceReporterStartAndClose(t *testing.T) {
@@ -13,4 +15,25 @@ func TestTraceReporterStartAndClose(t *testing.T) {
 		t.Fatal(err)
 	}
 	defer rcmgr.Close()
+}
+
+func TestConsumeEvent(t *testing.T) {
+	evt := rcmgr.TraceEvt{
+		Type:     rcmgr.TraceBlockAddStreamEvt,
+		Name:     "conn-1",
+		DeltaOut: 1,
+		Time:     time.Now().Format(time.RFC3339Nano),
+	}
+
+	err := view.Register(obs.DefaultViews...)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	str, err := obs.NewStatsTraceReporter()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	str.ConsumeEvent(evt)
 }
